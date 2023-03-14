ad11c228a151df539ec1b31f538a8ffd6d9e2f14
Ibrahima Ndiaye <ndb4509a@ens.fsi>
2023-03-14 18:16:02 +0100

    TP3 debut enume

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,11 @@
+package personnages;
+
+public enum Equipement {
+	CASQUE, BOUCLIER;
+	private String nom;
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+
+} 
+
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,13 +3,15 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private int force1;
+	private int force2;
 	
-	public Romain (String nom, int force) {
-		super();
+	
+	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert(this.force>=0):"Invalider user force";
 	}
-	
 	public String getNom() {
 		return nom;
 	}
@@ -19,17 +21,31 @@ public class Romain {
 	}
 	
 	private String prendreParole() {
-		return "Le romain" + nom + " : ";
+		return "Le romain " + nom + " : ";
 	}
-	
-	
 	
 	public void recevoirCoup(int forceCoup) {
+		assert(this.force>0):"invalide force entry";
+		force1 = force;
 		force -= forceCoup;
+		force2 = force;
+		assert(force1>force2):"post-condition";
 		if (force > 0) {
-		parler("Aïe");
+			parler("Aïe");
 		} else {
-		parler("J'abandonne...");
+			parler("J'abandonne...");
 		}
 	}
+	
+	public static void main(String[] args) {
+		
+		Romain minus = new Romain("Minus", 6);
+		System.out.println(minus.prendreParole());
+		minus.parler("Je suis Minus !");
+		minus.recevoirCoup(9);
+		
+		
+		
+		}
+
 }
